c7464fac1488b69fea3a625f308167f383ff011e
Shubham Kalihari <shubhamkalihari09@gmail.com>
2021-02-23 16:26:22 +0530

    functionality added: Update and delete post. Post can be updated and deleted by its author only new template created:  post.html update in code: create_post.html, home.html, routes.py New records added in Database: site.db

--- restroo/routes.py
+++ restroo/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from restroo import app, db, bcrypt
 from restroo.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from restroo.models import User, Post
@@ -110,4 +110,43 @@ def new_post():
         db.session.commit()
         flash('Your Post have been Created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        post.category = form.category.data
+        db.session.commit()
+        flash('Your Post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        form.category.data = post.category
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Post has been deleted!', 'success')
+    return redirect(url_for('home'))
